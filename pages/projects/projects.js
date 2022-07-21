c3b68f16310122507b9dfe9a65cde89bb9c5ee5f
Marcelo Lemes <marceloaflemes@gmail.com>
2022-07-21 20:09:57 -0300

    fix: ajustando url

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -1,4 +1,4 @@
-import { header } from "src/components/header/header.js";
+import { header } from "../../src/components/header/header.js";
 import { main } from "../../src/components/main-section/main-section.js";
 import { footer } from "../../src/components/footer/footer.js";
 import { listenProjectsElements } from "../../src/components/card-section/card-section.js";
