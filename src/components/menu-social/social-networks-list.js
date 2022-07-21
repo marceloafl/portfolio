9fca4b29b2c744aca9e88261052d44854747d209
Marcelo Lemes <marceloaflemes@gmail.com>
2022-07-21 19:52:31 -0300

    fix: ajustando url

--- src/components/menu-social/social-networks-list.js
+++ src/components/menu-social/social-networks-list.js
@@ -2,14 +2,14 @@ export const socialNetworks = {
     linkedin: {
         name: 'LinkedIn',
         page_url: 'https://www.linkedin.com/in/marceloafl/',
-        img_url: '../../src/assets/images/icons/linkedin-32px.png',
-        img_url_white: '../../src/assets/images/icons/linkedin-32px-light.png',
+        img_url: 'src/assets/images/icons/linkedin-32px.png',
+        img_url_white: 'src/assets/images/icons/linkedin-32px-light.png',
     },
     github: {
         name: 'Github',
         page_url: 'https://github.com/marceloafl',
-        img_url: '/src/assets/images/icons/github-mark-32px.png',
-        img_url_white: '/src/assets/images/icons/github-mark-32px-light.png',
+        img_url: 'src/assets/images/icons/github-mark-32px.png',
+        img_url_white: 'src/assets/images/icons/github-mark-32px-light.png',
     },
 }
 
