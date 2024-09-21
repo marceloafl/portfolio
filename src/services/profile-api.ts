92bcf7a5ddd0c6222081e03bcbdfc29b36fb2663
Marcelo Lemes <marceloaflemes@gmail.com>
2024-09-21 16:27:01 -0300

    feat: returning mocked profile

--- src/services/profile-api.ts
+++ src/services/profile-api.ts
@@ -2,11 +2,79 @@ import { IProfile } from "@/types/profiles";
 
 const url = "http://localhost:8081/v1/profiles";
 
+const mockedProfile: IProfile = {
+  name: "Marcelo Lemes",
+  email: "marceloaflemes@gmail.com",
+  githubUrl: "http://github.com/marceloafl",
+  linkedinUrl: "http://linkedin.com/in/marceloafl/",
+  title: "Software Developer",
+  subtitle: "Crafting transformative solutions that solve real-world issues.",
+  projects: [
+    {
+      name: "Birmingham Bank",
+      url: "http://www.birminghambank.com/",
+      stack: "Frontend",
+    },
+    {
+      name: "Grupo FCamara",
+      url: "http://fcamara.com/",
+      stack: "Fullstack",
+    },
+    {
+      name: "Rhopen",
+      url: "http://rhopen.com.br/",
+      stack: "Frontend",
+    },
+    {
+      name: "Café Cajubá",
+      url: "http://cafecajuba.com.br/",
+      stack: "Low-code",
+    },
+    {
+      name: "Munchkin",
+      url: "http://munchkin.com.br/",
+      stack: "Low-code",
+    },
+  ],
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "React.js",
+    "Next.js",
+    "Vue.js",
+    "Storybook",
+    "SEO",
+    "HTML",
+    "CSS",
+    "Tailwind",
+    "Styled-components",
+    "Module CSS",
+    "Bootstrap",
+    "Material-UI",
+    "Node.js",
+    "Express",
+    "C#",
+    ".NET",
+    "API Rest",
+    "Git",
+    "Vitest",
+    "Jest",
+    "React Testing Library",
+    "AWS",
+    "Azure",
+    "SQL",
+    "Google Analytics",
+    "Mendix",
+  ],
+};
+
 export async function getProfileByIdRequest(id: string) {
   try {
     const response = await fetch(`${url}/${id}`);
     const data = await response.json();
-    return data;
+
+    return mockedProfile;
+    // return data;
   } catch (error) {
     console.error(error);
   }
