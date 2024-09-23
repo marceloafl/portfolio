9df5aa7ba8050b0d090ab91dce984e3f18b3566b
Marcelo Lemes <marceloaflemes@gmail.com>
2024-09-23 10:46:43 -0300

    feat: implementing token verification in protected routes

--- src/utils/checkProtectedRoutes.ts
+++ src/utils/checkProtectedRoutes.ts
@@ -1,9 +1,16 @@
 import { permanentRedirect } from "next/navigation";
 import getAuthToken from "./getAuthToken";
+import jwt from "jsonwebtoken";
 
 export default function checkProtectedRoutes() {
   const token = getAuthToken();
   if (!token) {
+    permanentRedirect("/");
+  }
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
     permanentRedirect("/");
   }
 }
